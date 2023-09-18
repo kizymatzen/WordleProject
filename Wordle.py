832aff188e96e0c9df3ebfa439e79d69de03baf1
Kizy <kizymatzen@gmail.com>
2023-09-18 12:46:51 -0600

    deleting while loop so it doesn't run forever

--- Wordle.py
+++ Wordle.py
@@ -16,12 +16,12 @@ def wordle():
     gw = WordleGWindow() 
     guess_count = 0 # initialize the guess count
 
-    while guess_count < N_ROWS:
-        # pick a random word from FIVE_LETTER_WORDS
-        random_word = random.choice(FIVE_LETTER_WORDS)
-         # display the random_word in the first row
-        for col in range(N_COLS):
-            gw.set_square_letter(0, col, random_word[col])
+    # pick a random word from FIVE_LETTER_WORDS
+    random_word = random.choice(FIVE_LETTER_WORDS)    
+        
+    # display the random_word in the first row
+    for col in range(N_COLS):
+        gw.set_square_letter(0, col, random_word[col])
    
 
     def enter_action(s):
@@ -30,18 +30,18 @@ def wordle():
    # print(gw.get_current_row())
 
         current_row = gw.get_current_row()
-        
+
         # collect the word from the graphics window
         word = "".join([gw.get_square_letter(current_row, col) for col in range(N_COLS)]).lower()
 
         # check if the word is in the dictionary
         if word in FIVE_LETTER_WORDS:
             gw.show_message("Great job! That's a valid word.")
-            gw.set_current_row(current_row + 1) # move to the next row
-            guess_count += 1 # increment guess count
         else:
             gw.show_message("Not in word list.")
 
+        gw.set_current_row(current_row + 1) # move to the next row
+        guess_count += 1 # increment guess count
 
     # set up the enter key listener
     gw.add_enter_listener(enter_action)  
