4c2326a52e1392df436c5a55882fc9dfc62e82c9
BYUsweet28 <105523117+BYUsweet28@users.noreply.github.com>
2023-09-19 19:35:15 -0600

    Add files via upload

--- Wordle.py
+++ Wordle.py
@@ -14,20 +14,17 @@ def wordle():
 
     # create the game window
     gw = WordleGWindow() 
-    guess_count = 0 # initialize the guess count
 
-    # pick a random word from FIVE_LETTER_WORDS
-    random_word = random.choice(FIVE_LETTER_WORDS)    
-        
+     # pick a random word from FIVE_LETTER_WORDS
+    random_word = random.choice(FIVE_LETTER_WORDS)
+
     # display the random_word in the first row
-    for col in range(N_COLS):
-        gw.set_square_letter(0, col, random_word[col])
-   
+    # for col in range(N_COLS):
+    #     gw.set_square_letter(0, col, random_word[col])
 
     def enter_action(s):
 
-        nonlocal guess_count # allow modification of the guess count
-   # print(gw.get_current_row())
+        print(gw.get_current_row())
 
         current_row = gw.get_current_row()
 
@@ -37,14 +34,16 @@ def wordle():
         # check if the word is in the dictionary
         if word in FIVE_LETTER_WORDS:
             gw.show_message("Great job! That's a valid word.")
+            gw.set_current_row(current_row + 1)
         else:
             gw.show_message("Not in word list.")
 
-        gw.set_current_row(current_row + 1) # move to the next row
-        guess_count += 1 # increment guess count
 
     # set up the enter key listener
-    gw.add_enter_listener(enter_action)  
-    
+    gw.add_enter_listener(enter_action)
+
+
+# Startup code
+
 if __name__ == "__main__":
     wordle()
