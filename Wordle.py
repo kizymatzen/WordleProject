e2eea7e40bce3a6cb8e9c1c044058193b3fd3ae3
Brock Smiley <bsmiley2000@gmail.com>
2023-09-23 17:16:14 -0600

    added function that colors word row for each valid word. function recieves word, winning word, row, and gw(gameboard). in function for loops keeps track of position int he word and the letter. it then runs the functionality based on if letter is present in the winning word or if it is present and in the same index

--- Wordle.py
+++ Wordle.py
@@ -5,7 +5,14 @@ BE SURE TO UPDATE THIS COMMENT WHEN YOU WRITE THE CODE.
 
 import random
 from WordleDictionary import FIVE_LETTER_WORDS
-from WordleGraphics import WordleGWindow, N_COLS, N_ROWS
+from WordleGraphics import (
+    WordleGWindow,
+    N_COLS,
+    N_ROWS,
+    CORRECT_COLOR,
+    PRESENT_COLOR,
+    MISSING_COLOR,
+)
 
 
 def wordle():
@@ -20,14 +27,15 @@ def wordle():
         word = get_current_word(gw, current_row)
 
         if is_valid_word(word):
+            # color the word if it is valid. This will color the word even if they win too.
+            color_word_row(word, winning_word, current_row, gw)
             if word == winning_word:
                 gw.show_message("You win!")
                 gw.set_current_row(7)
-
             else:
                 total_guesses -= 1
                 gw.show_message(
-                    f"Great job! That's a valid word. {total_guesses} guesses left!"
+                    f"Great job that's a valid word! {total_guesses} guesses left!"
                 )
                 gw.set_current_row(current_row + 1)
         else:
@@ -42,6 +50,16 @@ def get_current_word(gw, row):
 
 def is_valid_word(word):
     return word in FIVE_LETTER_WORDS
+
+
+def color_word_row(word, winning_word, row, gw):
+    for index, letter in enumerate(word):
+        if letter == winning_word[index]:
+            gw.set_square_color(row, index, CORRECT_COLOR)
+        elif letter in winning_word:
+            gw.set_square_color(row, index, PRESENT_COLOR)
+        else:
+            gw.set_square_color(row, index, MISSING_COLOR)
 
 
 if __name__ == "__main__":
